63c594914a0302c3faaf5d96d4ead9232cffab17
NTD-Funix <ntd.funix@gmail.com>
2021-12-06 03:34:56 +0700

    This is third commit of asm2demo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> This is second updated
+        <p> This is third updated
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
